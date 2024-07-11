9237251bd6ae1df4b7f1a9fa700644869ff43455
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-07-11 09:02:33 +0600

    Add dynamodb table and connect it with creadintials.

--- signUp_project_with_flask/app.py
+++ signUp_project_with_flask/app.py
@@ -4,15 +4,23 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from dotenv import load_dotenv
 import os
 
+
+app = Flask(__name__)
+
 load_dotenv()
 
 aws_access_key = os.environ.get("AWS_ACCESS_KEY")
 aws_secret_key = os.environ.get("AWS_SECRET_KEY")
 aws_region = os.environ.get("REGION_NAME")
 
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=aws_region
+)
 
-app = Flask(__name__)
-
+table = dynamodb.Table('asignUp')
 
 
 @app.route('/')
